3f73b7239cb4b14732a7a890af5b93887bc7a5be
4815162342lost <family-gan@yandex.ru>
2022-03-26 14:50:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,33 @@ import requests
 import re
 import subprocess
 import tarfile
+import os
 
-def get_latest_version(): 
+def get_latest_version():
     r = requests.head("https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US")
     link = r.headers['Location']
     version_re = re.search("releases/.*/linux-x86_64", link)
     version = version_re.group(0).split('/')[1]
-    return link, version                  
+    return link, version
 
 def get_current_version():
-    firefox_run = subprocess.run(['/opt/firefox/firefox/firefox', '--version'], stdout=subprocess.PIPE)
+    firefox_run = subprocess.run([f"{os.environ['firefox_path']}firefox/firefox", '--version'], stdout=subprocess.PIPE)
     firefox_istalled_version = firefox_run.stdout.decode().rstrip().split(" ")[2]
     return firefox_istalled_version
 
 def download_firefox_archive(link, latest_version):
+    print("Downloading latest version...")
     r = requests.get(link)
     firefox_file = open(f"/tmp/{latest_version}.tar.bz2", "wb")
     firefox_file.write(r.content)
     firefox_file.close()
 
 def unpack_archive(latest_version):
+    print("Unpacking archive...")
     tar_file = tarfile.open(f"/tmp/{latest_version}.tar.bz2", "r:bz2")
-    tar_file.extractall("/tmp/firefox/")
+    tar_file.extractall(f"{os.environ['firefox_path']}")
     tar_file.close()
+    print("Completed")
 
 latest_version_link, latest_version = get_latest_version()
 my_version = get_current_version()
@@ -33,6 +37,7 @@ my_version = get_current_version()
 if latest_version == my_version:
     print(f"You have latest version: {my_version}")
 else:
-    print(f"New version released! Your version: {my_version}, latest version: {lstest_version}")
+    print(f"New version released! Your version: {my_version}, latest version: {latest_version}")
     download_firefox_archive(latest_version_link, latest_version)
     unpack_archive(latest_version)
+    print("Exiting... Bye!")
