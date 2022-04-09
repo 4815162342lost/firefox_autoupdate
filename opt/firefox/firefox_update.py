ccda237923ab6800e436e6b4b57bf3c0f15e240f
4815162342lost <family-gan@yandex.ru>
2022-04-09 14:10:41 +0300

    Update firefox_update.py
    Added kill firefox process
    Added "chunk downlods" to decrease memory usage

--- opt/firefox/firefox_update.py
+++ opt/firefox/firefox_update.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+import time
+
 import requests
 import re
 import subprocess
 import tarfile
 import os
+import psutil
+
 
 def get_latest_version():
+    """Get latest version of firefox"""
     try:
         r = requests.head("https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US")
     except Exception as e:
@@ -20,32 +25,67 @@ def get_latest_version():
         print(f"Status codes not OK while checking new version. Status code: {r.status_code}")
         exit(1)
 
+
 def get_current_version():
+    """Return current version of installed firefox"""
     firefox_run = subprocess.run([f"{os.environ['firefox_path']}firefox/firefox", '--version'], stdout=subprocess.PIPE)
-    firefox_istalled_version = firefox_run.stdout.decode().rstrip().split(" ")[2]
-    return firefox_istalled_version
+    firefox_installed_version = firefox_run.stdout.decode().rstrip().split(" ")[2]
+    return firefox_installed_version
+
 
 def download_firefox_archive(link, latest_version):
+    """Download latest firefox version and save to /tmp/"""
     print("Downloading latest version...")
     try:
-        r = requests.get(link)
+        r = requests.get(link , stream=True)
     except Exception as e:
         print(f"Error while download... Exception: {e}")
         exit(1)
     if r.status_code == requests.codes.ok:
         firefox_file = open(f"/tmp/{latest_version}.tar.bz2", "wb")
-        firefox_file.write(r.content)
-        firefox_file.close()
+        try:
+            for current_chunk in r.iter_content(5242880):
+                print("Chunk")
+                firefox_file.write(current_chunk)
+            firefox_file.close()
+        except Exception as e:
+            print(f"Error while downloading file. Exception: {e}")
+            exit(1)
     else:
         print(f"Error when download... Status code: {r.status_code}")
         exit(1)
 
+
+def find_running_firefox_pids():
+    """Find main PID running firefox process"""
+    processes_list = psutil.process_iter()
+    for process in processes_list:
+        if process.name() == "firefox-bin":
+            print(f"Found firefox process. PID: {process.pid}")
+            kill_running_firefox_instance(process)
+            return None
+    print("Running firefox not found")
+
+
+def kill_running_firefox_instance(process):
+    """Send SIGQUIT to firefox pid"""
+    print("Going to kill firefox process. After we will sleep 10 seconds")
+    try:
+        process.send_signal(1)
+    except Exception as e:
+        print(f"Can not kill running Firefox instance. Error: {e}")
+        exit(1)
+    time.sleep(10)
+
+
 def unpack_archive(latest_version):
+    """Unpack archive"""
     print("Unpacking archive...")
     tar_file = tarfile.open(f"/tmp/{latest_version}.tar.bz2", "r:bz2")
     tar_file.extractall(f"{os.environ['firefox_path']}")
     tar_file.close()
     print("Completed")
+
 
 latest_version_link, latest_version = get_latest_version()
 my_version = get_current_version()
@@ -55,5 +95,6 @@ if latest_version == my_version:
 else:
     print(f"New version released! Your version: {my_version}, latest version: {latest_version}")
     download_firefox_archive(latest_version_link, latest_version)
+    find_running_firefox_pids()
     unpack_archive(latest_version)
     print("Exiting... Bye!")
